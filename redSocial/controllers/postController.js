cb6918bda74a14c41df1267532b77653420d4f4a
ktabanera <90622102+ktabanera@users.noreply.github.com>
2021-09-26 19:59:19 -0300

    mas cambios

--- redSocial/controllers/postController.js
+++ redSocial/controllers/postController.js
@@ -1,12 +1,19 @@
 let post= require("../data/post")
+let user= require("../data/user")
+let comments= require("../data/comments")
 const postController ={
     add: function (req, res) {
         return res.render('agregarPost');
     },
     detalle: function (req, res) {
         return res.render('detallePost',{
-            posts: post.lista,
+            posts: post.lista[1], 
+            usuario: user.lista,
+            comentario: comments.lista,
+
+
         });
+
     }
 }
 module.exports = postController;
