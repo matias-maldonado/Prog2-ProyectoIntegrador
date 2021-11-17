5f5bbb9816fb7d4725d07e9f59d684f5f6d341f3
matias-maldonado <63106003+matias-maldonado@users.noreply.github.com>
2021-11-17 16:10:04 -0300

    registre + multer :+1:

--- redSocial/controllers/usersControllers.js
+++ redSocial/controllers/usersControllers.js
@@ -8,6 +8,19 @@ const loginController ={
     register: function (req, res) {
         return res.render('registracion');
     },
+    store: function (req, res) {
+        db.User.create({
+            email: req.body.email,
+            userName: req.body.userName,
+            img: req.file.filename,
+            fecha:req.body.fecha,
+            password:req.body.password,
+            age: req.body.age,
+        })
+        .then(user=>{
+            return res.redirect ('/users/login')
+        })
+    },
     profile: function (req, res) {
         db.User.findByPk (req.params.id,{
             include: [
--- redSocial/routes/users.js
+++ redSocial/routes/users.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 let userController = require ('../controllers/usersControllers')
 /* GET users listing. */
+const multer = require('multer');
+const path = require('path');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/img/users')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+  })
+  
+  const upload = multer({ storage: storage })
 router.get('/login', userController.login)
 router.get('/profile/:id', userController.profile)
 router.get('/register', userController.register)
+router.post('/register',upload.single('img'), userController.store)
 router.get('/edit', userController.edit)
 module.exports = router;
